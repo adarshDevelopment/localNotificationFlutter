fc07fb9b288776550114523789436801bb96b20d
adarshDevelopment <152478141+adarshDevelopment@users.noreply.github.com>
2023-12-27 15:53:58 +0545

    without gradle manipulation

---
+++ lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Local Notifications'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            ElevatedButton(
+                onPressed: () {}, child: const Icon(Icons.notifications_outlined))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/local_notifications.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotifications {
+  static final FlutterLocalNotificationsPlugin
+      _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  // initialize the local notifications
+  static Future init() async {
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+
+    //android initialization settings
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings(
+            '@mipmap/ic_launcher'); // default icon from ANdroidManifest.xml. android/app/src/main/..
+
+    // ios android initialization settings
+    final DarwinInitializationSettings initializationSettingsDarwin =
+        DarwinInitializationSettings(
+      onDidReceiveLocalNotification: (id, title, body, payload) {},
+    );
+
+    // linux android initialization settings
+    const LinuxInitializationSettings initializationSettingsLinux =
+        LinuxInitializationSettings(defaultActionName: 'Open notification');
+
+    // android initialization settings
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsDarwin,
+            linux: initializationSettingsLinux);
+
+    // initialize method from FlutterLocalNotificationsPlugin
+    _flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (details) {},
+    );
+  }
+
+  //show a simple notification
+  static Future showSimpleNotification({
+    required String title,
+    required String body,
+    required String payload,
+  }) async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('channelId', 'channelName',
+            channelDescription: 'Channel description',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+
+    const NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+      iOS: null,
+    );
+
+    await _flutterLocalNotificationsPlugin
+        .show(2, title, body, notificationDetails, payload: payload);
+  }
+}
